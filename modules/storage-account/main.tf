ca78455037d512d9a2f41a652daee7dac5fb4677
illuruyadhuvamsi1-web <illuruyadhuvamsi1@gmail.com>
2026-01-09 17:41:24 +0530

    first commit

---
+++ modules/storage-account/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_storage_account" "stg" {
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = var.account_replication_type
+  lifecycle {
+    precondition {
+    condition     = var.account_replication_type == "LRS"
+    error_message = "Policy violation: Storage Account replication type must be LRS."
+  }
+  } 
+}
+
